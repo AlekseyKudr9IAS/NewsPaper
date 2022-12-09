6e78d862a6b5257817df66152df0aea78c346cee
AlekseyK <Kudr9IAS140887@gmail.com>
2022-12-09 21:58:07 +0300

    First commit

---
+++ news/notes.py
@@ -0,0 +1,26 @@
+Politics = "PO"
+Business = "BU"
+Science = "SC"
+Health = "HE"
+Sports = "SP"
+Arts = "AR"
+Travel = "TR"
+
+category = [
+    (Politics, "Политика"),
+    (Business, "Бизнес"),
+    (Science, "Наука"),
+    (Health, "Здоровье"),
+    (Sports, "Спорт"),
+    (Arts, "Искусство"),
+    (Travel, "Путешествие")
+]
+
+
+article = "AR"
+news = "NE"
+
+type_post = [
+    (article, "Статья"),
+    (news, "Новость"),
+]
